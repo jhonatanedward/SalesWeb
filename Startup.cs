aad5aa148c3a0e48f833a26e5aa0c400c86f6cd3
Jhonatan Edward <jon.edward@outlook.com>
2020-07-28 22:29:45 -0300

    Criada: Classe: SellerService, View: Index do SellerController e adicionada injeção de dependencia.

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -34,6 +30,7 @@ namespace SalesWebMvc
             });
 
             services.AddScoped<SeendingService>();
+            services.AddScoped<SellerService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
